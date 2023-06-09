76200bf1d2bc8681690c27b93f35295bbbef91ef
Aman Khan <120627921+Amankh00@users.noreply.github.com>
2023-06-09 17:47:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-function toggleNav() {
-  var nav = document.getElementById("myNav");
-  nav.classList.toggle("active");
-}
+const cart = document.getElementById('cart');
+var count = 0;
+cart.innerHTML = count;
+
+const handleIncrement = () => {
+  count++;
+  cart.innerHTML = count;
+};
+
+const buttons = document.querySelectorAll(".sec4 .best_perfume button");
+buttons.forEach((button) => {
+  button.addEventListener("click", handleIncrement);
+});
