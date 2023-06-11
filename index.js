0004d2e47d1f6594671f6a4fdf8e1ec3ae4a9223
Aman Khan <120627921+Amankh00@users.noreply.github.com>
2023-06-11 14:33:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+
+
 const cart = document.getElementById('cart');
 var count = 0;
-cart.innerHTML = count;
+cart.innerHTML =count;
 
-const handleIncrement = () => {
+const handleIncrement = (event) => {
+  const button = event.target;
+  
+  if (button.dataset.clicked === 'true') {
+    // button.innerHTML = 'Already added';
+    alert("ALREADY ADDED , THANK YOU ")
+    return;
+  }
+  
   count++;
   cart.innerHTML = count;
+  button.innerHTML = 'Added';
+  button.dataset.clicked = 'true';
 };
-
 const buttons = document.querySelectorAll(".sec4 .best_perfume button");
 buttons.forEach((button) => {
   button.addEventListener("click", handleIncrement);
-});
+})
